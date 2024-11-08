bfecb46cc703e50d7d17242f8e9d58d1c48c61b5
Fadinuj <fadi.nujedat@msmail.ariel.ac.il>
2024-11-08 13:59:18 +0000

    updated

---
+++ Move.java
@@ -0,0 +1,15 @@
+public class Move {
+
+    public Move() {}
+
+
+    public Position position()
+    {
+        return null;
+    }
+
+    public Disc disc()
+    {
+        return null;
+    }
+}
---
+++ Position.java
@@ -0,0 +1,18 @@
+public class Position {
+    int row;
+    int col;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int row() {
+        return row;
+    }
+
+    public int col() {
+        return col;
+    }
+
+}
